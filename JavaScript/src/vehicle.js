a4c11397e92b1cae55876d730eaf2e6434e1dd42
Soufyan <soufyan.najjar@student.ehb.be>
2021-05-22 14:28:57 +0200

    feat(class): added vehicle class

---
+++ JavaScript/src/vehicle.js
@@ -0,0 +1,7 @@
+export function Vehicle(brand, model, nbDoors, price, releaseYear) {
+    this.brand = brand;
+    this.model = model;
+    this.nbDoors = nbDoors;
+    this.price = price;
+    this.releaseYear = releaseYear;
+}
