7d933525b2f7a65920b36ba24e3f52225dd6c183
Soufyan <soufyan.najjar@student.ehb.be>
2021-05-22 15:23:52 +0200

    feat(class): added different vehicle subclasses

---
+++ Java/src/main/java/vehicle/Audi.java
@@ -0,0 +1,11 @@
+package vehicle;
+
+public class Audi extends Vehicle{
+    public Audi() {
+        setBrand("Audi");
+        setModel("A5");
+        setPrice(25000d);
+        setNbDoors(3);
+        setReleaseYear(2016);
+    }
+}
---
+++ Java/src/main/java/vehicle/BMW.java
@@ -0,0 +1,11 @@
+package vehicle;
+
+public class BMW extends Vehicle{
+    public BMW() {
+        setBrand("BMW");
+        setModel("Serie 5");
+        setPrice(19000d);
+        setNbDoors(5);
+        setReleaseYear(2017);
+    }
+}
